4bd29ddc5dc2e78fcb02dee1325b7ce601bbf790
quails4Eva <dpetchey@gmail.com>
2016-02-15 22:04:21 +0000

    Add problem 7.1

---
+++ 7.1/Problem7_1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1 {
+    public static class Problem7_1 {
+
+        public static int StringToInt(string input) {
+
+            if (string.IsNullOrWhiteSpace(input)) {
+                return 0;
+            }
+
+            int output = 0;
+            int parity = input[0] == '-' ? -1 : 1;
+
+            int index = 1;
+
+            foreach (var character in input.SkipWhile(c => c == '-').Reverse()) {
+
+                output += (character - '0') * index;
+                index *= 10;
+            }
+
+            return output * parity;
+        }
+
+        public static string IntToString(int input) {
+
+            if (input == 0) {
+                return "0";
+            }
+
+            StringBuilder output = new StringBuilder();
+
+            bool isNegative = (input < 0);
+
+            input = Math.Abs(input);
+
+            while (input > 0) {
+
+                output.Append((input % 10));
+                input /= 10;
+            }
+
+            if (isNegative) {
+                output.Append('-');
+            }
+
+            var outputArray = new string[output.Length];
+
+            return new string(output.ToString().Reverse().ToArray()); // Could probably do this more efficiently using manual array allocation
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using _7._1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class UnitTest1 {
+
+        [TestMethod]
+        public void Test7_1_StringToInt() {
+
+            Assert.AreEqual(100, Problem7_1.StringToInt("100"));
+            Assert.AreEqual(134, Problem7_1.StringToInt("134"));
+            Assert.AreEqual(982, Problem7_1.StringToInt("982"));
+            Assert.AreEqual(43890453, Problem7_1.StringToInt("43890453"));
+        }
+
+        [TestMethod]
+        public void Test7_1_StringToIntNegative() {
+
+            // Negative
+            Assert.AreEqual(-43489, Problem7_1.StringToInt("-43489"));
+        }
+
+        [TestMethod]
+        public void Test7_1_StringToInt0() {
+
+            // Negative
+            Assert.AreEqual(-0, Problem7_1.StringToInt("-0"));
+            Assert.AreEqual(0, Problem7_1.StringToInt("0"));
+        }
+
+        [TestMethod]
+        public void Test7_1_IntToString() {
+
+            Assert.AreEqual("100", Problem7_1.IntToString(100));
+            Assert.AreEqual("134", Problem7_1.IntToString(134));
+            Assert.AreEqual("982", Problem7_1.IntToString(982));
+            Assert.AreEqual("43890453", Problem7_1.IntToString(43890453));
+        }
+
+        [TestMethod]
+        public void Test7_1_IntToStringNegative() {
+
+            // Negative
+            Assert.AreEqual("-43489", Problem7_1.IntToString(-43489));
+        }
+
+        [TestMethod]
+        public void Test7_1_IntToString0() {
+
+            // Negative
+            Assert.AreEqual("0", Problem7_1.IntToString(-0));
+            Assert.AreEqual("0", Problem7_1.IntToString(0));
+        }
+    }
+}
