a10bb9f2388044fb207fa067e4dc4ee226f6cc9a
quails4Eva <dpetchey@gmail.com>
2016-02-28 21:15:11 +0000

    Problem 12.1

---
+++ 12.1/Problem12_1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12._1 {
+    public static class Problem12_1 {
+
+        public static int BinarySearchFirstInstance(int[] array, int toFind, int min = 0, int? max = null) {
+
+            if (!max.HasValue) {
+
+                max = array.Length - 1;
+            }
+
+            var midway = (min + max.Value) / 2;
+            
+            if (array[midway] == toFind) {
+                if (midway <= min) {
+
+                    return min;
+                }
+                else {
+
+                    return BacktrackForFirstElement(array, toFind, midway, min, midway - 1);
+                }
+            }
+            else if (min >= max) {
+
+                return -1;
+            }
+            else if (array[midway] < toFind) {
+
+                return BinarySearchFirstInstance(array, toFind, midway + 1, max);
+            }
+            else {
+
+                return BinarySearchFirstInstance(array, toFind, min, midway - 1);
+            }
+        }
+
+        private static int BacktrackForFirstElement(int[] array, int toFind, int minFound, int min, int max) {
+
+            if (min == max) {
+
+                return array[min] == toFind ? min : minFound;
+            }
+            else {
+
+                var midway = (min + max) / 2;
+
+                if (array[midway] == toFind) {
+
+                    return BacktrackForFirstElement(array, toFind, midway, min, midway - 1);
+                }
+                else {
+                    return BacktrackForFirstElement(array, toFind, minFound, midway + 1, max);
+                }
+            }
+        }
+    }
+}
---
+++ Tests/Test12_1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _12._1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class Test12_1 {
+
+        [TestMethod]
+        public void ReturnMinus1IfNotPresent() {
+
+            Assert.AreEqual(-1, Problem12_1.BinarySearchFirstInstance(new[] { 12, }, 1));
+            Assert.AreEqual(-1, Problem12_1.BinarySearchFirstInstance(new[] { 2, 12, }, 1));
+            Assert.AreEqual(-1, Problem12_1.BinarySearchFirstInstance(new[] { 1, 2, 3, 5, }, 4));
+            Assert.AreEqual(-1, Problem12_1.BinarySearchFirstInstance(new[] { 1, 12, }, -1));
+            Assert.AreEqual(-1, Problem12_1.BinarySearchFirstInstance(new[] { -1, 1, 12, }, 0));
+        }
+
+        [TestMethod]
+        public void UniqueElementFound() {
+
+            Assert.AreEqual(0, Problem12_1.BinarySearchFirstInstance(new[] { 1, }, 1));
+            Assert.AreEqual(1, Problem12_1.BinarySearchFirstInstance(new[] { 2, 12, }, 12));
+            Assert.AreEqual(3, Problem12_1.BinarySearchFirstInstance(new[] { 1, 2, 3, 4, 5, }, 4));
+            Assert.AreEqual(0, Problem12_1.BinarySearchFirstInstance(new[] { -1, 1, 12, }, -1));
+            Assert.AreEqual(1, Problem12_1.BinarySearchFirstInstance(new[] { -1, 0, 1, 12, }, 0));
+        }
+
+
+        [TestMethod]
+        public void MultipleCopiesOfElementFound() {
+
+            Assert.AreEqual(0, Problem12_1.BinarySearchFirstInstance(new[] { 1, 1, }, 1));
+            Assert.AreEqual(1, Problem12_1.BinarySearchFirstInstance(new[] { 2, 12, 12, 12, }, 12));
+            Assert.AreEqual(5, Problem12_1.BinarySearchFirstInstance(new[] { 1, 1, 2, 2, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, }, 4));
+            Assert.AreEqual(0, Problem12_1.BinarySearchFirstInstance(new[] { -1, -1, 1, 1, 1, 5, 12, 12, }, -1));
+            Assert.AreEqual(1, Problem12_1.BinarySearchFirstInstance(new[] { -1, 0, 0, 1, 1, 1, 4, 12, 12, }, 0));
+        }
+    }
+}
