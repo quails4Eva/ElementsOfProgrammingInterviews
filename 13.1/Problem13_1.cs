ef4e0bef3fb02766069158a33c9f1592486f79e5
quails4Eva <dpetchey@gmail.com>
2016-03-02 23:03:01 +0000

    Problem 13.1

---
+++ 13.1/Problem13_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._1 {
+    public class Problem13_1 {
+
+        public static Dictionary<string, List<string>> GroupStrings(IEnumerable<string> input) {
+
+            Dictionary<string, List<string>> groupedStrings = new Dictionary<string, List<string>>(input.Count());
+
+            foreach (var val in input) {
+
+                var key = new string(val.OrderBy(c => c).ToArray());
+
+                List<string> currentMatches;
+
+                if (!groupedStrings.TryGetValue(key, out currentMatches)) {
+
+                    currentMatches = new List<string>();
+                    groupedStrings.Add(key, currentMatches);
+                }
+                currentMatches.Add(val);
+            }
+
+            return groupedStrings.Where(kvp => kvp.Value.Count > 1).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+            //return input.GroupBy(s => new string(s.OrderBy(c => c).ToArray())).Where(g => g.Count() > 1).ToDictionary(g => g.Key, global => global.ToList());
+        }
+    }
+}
---
+++ 13.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13._1 {
+    class Program {
+        static void Main(string[] args) {
+
+            var test = Problem13_1.GroupStrings(
+                new[] {
+                    "debitcard", "elvis", "silent", "badcredit", "lives", "freedom", "listen", "levis", "money",
+                }
+            );
+
+        }
+    }
+}
