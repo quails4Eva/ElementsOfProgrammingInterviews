39689094d6bd2b3d47a00e64498d02e173da7203
quails4Eva <dpetchey@gmail.com>
2016-03-03 22:19:40 +0000

    Use Linq method for comparing arrays

--- Tests/Test14_1.cs
+++ Tests/Test14_1.cs
@@ -1,4 +1,5 @@
 ﻿using _14._1;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Tests {
@@ -10,7 +11,7 @@ namespace Tests {
             var array1 = new[] { 1, 1, 1, 2, 3, 4, 5, 7, 7, 10 };
             var array2 = new[] { 1, 1, 2, 2, 3, 4, 6, 7, 9, 9, 10, 11, 11, 25 };
 
-            Assert.IsTrue(Problem14_1.GetSortedArrayIntersection(array1, array2).ArraysEqual(Problem14_1.GetSortedArrayIntersection1(array1, array2)));
+            Assert.IsTrue(Enumerable.SequenceEqual(Problem14_1.GetSortedArrayIntersection(array1, array2), Problem14_1.GetSortedArrayIntersection1(array1, array2)));
         }
     }
 }
