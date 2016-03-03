46785e9c966e44571f0bfacd66414b406b997f9c
quails4Eva <dpetchey@gmail.com>
2016-03-03 20:57:34 +0000

    Naive implementation of 14.1

---
+++ 14.1/Problem14_1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14._1 {
+    public static class Problem14_1 {
+        public static int[] GetSortedArrayIntersection(int[] array1, int[] array2) {
+
+            return new HashSet<int>(array1.Concat(array2)).ToArray();
+        }
+    }
+}
