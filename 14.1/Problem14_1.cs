16f50450f3d7cdb146a7a3e249615dadecc66113
quails4Eva <dpetchey@gmail.com>
2016-03-03 21:35:22 +0000

    More low tech solution to 14.1

--- 14.1/Problem14_1.cs
+++ 14.1/Problem14_1.cs
@@ -8,7 +8,40 @@ namespace _14._1 {
     public static class Problem14_1 {
         public static int[] GetSortedArrayIntersection(int[] array1, int[] array2) {
 
-            return new HashSet<int>(array1.Concat(array2)).ToArray();
+            List<int> elements = new List<int>();
+
+            int i = 0;
+            int j = 0;
+            int? lastElement = null;
+
+            while (i < array1.Length || j < array2.Length) {
+
+                var val1 = i == array1.Length ? (int?)null : array1[i];
+                var val2 = j == array2.Length ? (int?)null : array2[j];
+
+                if (val2 == null || val1 < val2) {
+                    if (val1 != lastElement) {
+
+                        elements.Add(val1.Value);
+                        lastElement = val1;
+                    }
+                    i++;
+                }
+                else {
+                    if (val2 != lastElement) {
+
+                        elements.Add(val2.Value);
+                        lastElement = val2;
+                    }
+                    j++;
+                }
+            }
+
+            return elements.ToArray();
+        }
+        public static int[] GetSortedArrayIntersection1(int[] array1, int[] array2) {
+            
+            return new HashSet<int>(array1.Concat(array2)).OrderBy(i => i).ToArray();
         }
     }
 }
---
+++ Tests/Test14_1.cs
@@ -0,0 +1,16 @@
+﻿using _14._1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class Test14_1 {
+        [TestMethod]
+        public void Test14_1Intersection() {
+
+            var array1 = new[] { 1, 1, 1, 2, 3, 4, 5, 7, 7, 10 };
+            var array2 = new[] { 1, 1, 2, 2, 3, 4, 6, 7, 9, 9, 10, 11, 11, 25 };
+
+            Assert.IsTrue(Problem14_1.GetSortedArrayIntersection(array1, array2).ArraysEqual(Problem14_1.GetSortedArrayIntersection1(array1, array2)));
+        }
+    }
+}
