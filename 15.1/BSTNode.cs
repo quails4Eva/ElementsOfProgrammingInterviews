c3e81d0b42dd9385f241eba2fd4799897da90c8f
quails4Eva <dpetchey@gmail.com>
2016-03-05 14:25:25 +0000

    Problem 15.1

---
+++ 15.1/BSTNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._1 {
+    public class BSTNode<T> where T : IComparable<T> {
+
+        public T Data { get; set; }
+        public BSTNode<T> Left { get; set; }
+        public BSTNode<T> Right { get; set; }
+
+        public bool IsBSTSatisfied() {
+
+            return this.IsBSTSatisfied(default(T), false, default(T), false);
+        }
+
+        // We have a useMax so that we don't have to pass in the maximum value for a generic type (which may not exist)
+        private bool IsBSTSatisfied(T max, bool useMax, T min, bool useMin) {
+
+            var satisfiesMax = (!useMax || this.Data.CompareTo(max) <= 0);
+            var satisfiesMin = (!useMin || this.Data.CompareTo(min) >= 0);
+
+            var satisfiesLeft = this.Left == null || (this.Left.IsBSTSatisfied(this.Data, true, min, useMin));
+            var satisfiesRight = this.Right == null || (this.Right.IsBSTSatisfied(max, useMax, this.Data, true));
+
+            return satisfiesMax && satisfiesMin && satisfiesLeft && satisfiesRight;
+        }
+    }
+}
---
+++ Tests/Test15_1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _15._1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class Test15_1 {
+
+        [TestMethod]
+        public void TestEmpty() {
+
+            var testData = new BSTNode<int>() { Data = 1 };
+
+            Assert.IsTrue(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestLeftNodeSmaller() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Left = new BSTNode<int>() { Data = 0, }
+            };
+
+            Assert.IsTrue(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestLeftNodeEquals() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Left = new BSTNode<int>() { Data = 1, }
+            };
+
+            Assert.IsTrue(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestLeftNodeGreater() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Left = new BSTNode<int>() { Data = 2, }
+            };
+
+            Assert.IsFalse(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestLeftTreeSmaller() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Left = new BSTNode<int>() {
+                    Data = 0,
+                    Left = new BSTNode<int>() {
+
+                        Data = -1,
+                    }
+                }
+            };
+
+            Assert.IsTrue(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestLeftTreeGreater() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Left = new BSTNode<int>() {
+                    Data = 0,
+                    Left = new BSTNode<int>() {
+
+                        Data = 2,
+                    }
+                }
+            };
+
+            Assert.IsFalse(testData.IsBSTSatisfied());
+        }
+
+
+
+
+        [TestMethod]
+        public void TestRightNodeSmaller() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Right = new BSTNode<int>() { Data = 0, }
+            };
+
+            Assert.IsFalse(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestRightNodeEquals() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Right = new BSTNode<int>() { Data = 1, }
+            };
+
+            Assert.IsTrue(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestRightNodeGreater() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Right = new BSTNode<int>() { Data = 2, }
+            };
+
+            Assert.IsTrue(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestRightTreeSmaller() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Right = new BSTNode<int>() {
+                    Data = 1,
+                    Right = new BSTNode<int>() {
+
+                        Data = -1,
+                    }
+                }
+            };
+
+            Assert.IsFalse(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestRightTreeGreater() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Right = new BSTNode<int>() {
+                    Data = 1,
+                    Right = new BSTNode<int>() {
+
+                        Data = 2,
+                    }
+                }
+            };
+
+            Assert.IsTrue(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestMaxContidion() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Left = new BSTNode<int>() {
+                    Data = -10,
+                    Right = new BSTNode<int>() {
+
+                        Data = 2,
+                    }
+                }
+            };
+
+            Assert.IsFalse(testData.IsBSTSatisfied());
+        }
+
+        [TestMethod]
+        public void TestMinContidion() {
+
+            var testData = new BSTNode<int>() {
+                Data = 1,
+                Right = new BSTNode<int>() {
+                    Data = 10,
+                    Left = new BSTNode<int>() {
+
+                        Data = 0,
+                    }
+                }
+            };
+
+            Assert.IsFalse(testData.IsBSTSatisfied());
+        }
+        // Should also do some tests on larger data structures to check that the recursion works fully
+    }
+}
