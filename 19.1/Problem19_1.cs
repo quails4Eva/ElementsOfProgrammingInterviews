2b42970c22df961944a5cafc1aca325f8064aa57
quails4Eva <dpetchey@gmail.com>
2016-03-06 18:32:01 +0000

    Problem 19.1

---
+++ 19.1/Problem19_1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19._1 {
+    public static class Problem19_1 {
+
+        public static bool ArrayHasPath(bool[,] maze, Tuple<int, int> currentPosition, Tuple<int, int> endPosition) {
+
+            return ArrayHasPath(maze, currentPosition, endPosition, new HashSet<Tuple<int, int>>());
+        }
+
+
+        private static bool ArrayHasPath(bool[,] maze, Tuple<int, int> currentPosition, Tuple<int, int> endPosition, HashSet<Tuple<int, int>> visitedLocations) {
+
+            if (currentPosition.Item1 == endPosition.Item1 && currentPosition.Item2 == endPosition.Item2) {
+                return true;
+            }
+            else if (visitedLocations.Contains(currentPosition)) {
+                return false;
+            }
+
+            visitedLocations.Add(currentPosition);
+
+            var x = currentPosition.Item1;
+            var y = currentPosition.Item2;
+
+            if (x > 0 && maze[x - 1, y] && ArrayHasPath(maze, Tuple.Create(x - 1, y), endPosition, visitedLocations)) {
+
+                return true;
+            }
+            else if (x < maze.GetLength(0) - 1 && maze[x + 1, y] && ArrayHasPath(maze, Tuple.Create(x + 1, y), endPosition, visitedLocations)) {
+
+                return true;
+            }
+            else if (y > 0 && maze[x, y - 1] && ArrayHasPath(maze, Tuple.Create(x, y - 1), endPosition, visitedLocations)) {
+
+                return true;
+            }
+            else if (y < maze.GetLength(1) - 1 && maze[x, y + 1] && ArrayHasPath(maze, Tuple.Create(x, y + 1), endPosition, visitedLocations)) {
+
+                return true;
+            }
+
+            visitedLocations.Remove(currentPosition);
+
+            return false;
+        }
+    }
+}
---
+++ Tests/Test19_1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _19._1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+    [TestClass]
+    public class Test19_1 {
+
+        [TestMethod]
+        public void TestHasExit() {
+
+            var array = new bool[,] { // x and y are opposite of their intuitive visual representation when thinking of the indicies as x,y coordinates
+                { false, false, true, }, // x = 0
+                { false, true, true, }, // x = 1
+                { true, true, false, }, // x = 2
+            };
+
+            Assert.IsTrue(Problem19_1.ArrayHasPath(array, Tuple.Create(0, 2), Tuple.Create(2, 0)));
+        }
+
+        [TestMethod]
+        public void TestHasExit1() {
+
+            var array = new bool[,] { // x and y are opposite of their intuitive visual representation when thinking of the indicies as x,y coordinates
+                { true, true, true, }, // x = 0
+                { true, false, true, }, // x = 1
+                { true, true, true, }, // x = 2
+            };
+
+            Assert.IsTrue(Problem19_1.ArrayHasPath(array, Tuple.Create(0, 2), Tuple.Create(2, 0)));
+        }
+
+        [TestMethod]
+        public void TestHasExit2() {
+
+            var array = new bool[,] { // x and y are opposite of their intuitive visual representation when thinking of the indicies as x,y coordinates
+                { true, false, true, }, // x = 0
+                { true, false, true, }, // x = 1
+                { true, true, true, }, // x = 2
+            };
+
+            Assert.IsTrue(Problem19_1.ArrayHasPath(array, Tuple.Create(0, 0), Tuple.Create(0, 2)));
+        }
+
+        [TestMethod]
+        public void TestHasExit3() {
+
+            var array = new bool[,] { // x and y are opposite of their intuitive visual representation when thinking of the indicies as x,y coordinates
+                { true, false, true, true, }, // x = 0
+                { true, false, true, true, }, // x = 1
+                { true, true, true, true, }, // x = 2
+                { true, true, false, true, }, // x = 3
+            };
+
+            Assert.IsTrue(Problem19_1.ArrayHasPath(array, Tuple.Create(0, 0), Tuple.Create(3, 3)));
+        }
+
+        [TestMethod]
+        public void TestHasNoExit() {
+
+            var array = new bool[,] { // x and y are opposite of their intuitive visual representation when thinking of the indicies as x,y coordinates
+                { true, false, true, }, // x = 0
+                { true, false, true, }, // x = 1
+                { true, false, true, }, // x = 2
+            };
+
+            Assert.IsFalse(Problem19_1.ArrayHasPath(array, Tuple.Create(0, 0), Tuple.Create(0, 2)));
+        }
+
+        [TestMethod]
+        public void TestHasNoExit2() {
+
+            var array = new bool[,] { // x and y are opposite of their intuitive visual representation when thinking of the indicies as x,y coordinates
+                { true, false, true, }, // x = 0
+                { true, false, false, }, // x = 1
+                { true, true, true, }, // x = 2
+            };
+
+            Assert.IsFalse(Problem19_1.ArrayHasPath(array, Tuple.Create(0, 0), Tuple.Create(0, 2)));
+        }
+
+        [TestMethod]
+        public void TestHasNoExitExitIsWall() {
+
+            var array = new bool[,] { // x and y are opposite of their intuitive visual representation when thinking of the indicies as x,y coordinates
+                { true, false, false, }, // x = 0
+                { true, false, true, }, // x = 1
+                { true, true, true, }, // x = 2
+            };
+
+            Assert.IsFalse(Problem19_1.ArrayHasPath(array, Tuple.Create(0, 0), Tuple.Create(0, 2)));
+        }
+
+        [TestMethod]
+        public void TestHasNoExit3() {
+
+            var array = new bool[,] { // x and y are opposite of their intuitive visual representation when thinking of the indicies as x,y coordinates
+                { true, false, true, false }, // x = 0
+                { true, false, false, true }, // x = 1
+                { true, true, true, false }, // x = 2
+                { true, true, true, false }, // x = 3
+            };
+
+            Assert.IsFalse(Problem19_1.ArrayHasPath(array, Tuple.Create(0, 0), Tuple.Create(0, 2)));
+        }
+    }
+}
